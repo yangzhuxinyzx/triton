5d0be89777622ad6d55af03d3a5c2eb8b9fd903e
Jokeren <robinho364@gmail.com>
2026-08-31 16:18:04 -0400

    Move AMD Proton lowering before warp specialization

--- third_party/amd/backend/compiler.py
+++ third_party/amd/backend/compiler.py
@@ -408,6 +408,8 @@ class HIPBackend(BaseBackend):
         ##    For now it is used as a controller for developers only.
         __HIP_FTZ = True
         amd.passes.ttgpuir.add_to_llvmir(pm, options.arch, __HIP_FTZ)
+        # Lower Proton segment values before warp specialization captures partition operands.
+        instrument(pm, name="proton", point="proton-to-llvm", options=options)
         amd.passes.ttgpuir.add_warp_specialize_to_llvm(pm, options.arch)
         passes.common.add_canonicalizer(pm)
         passes.common.add_cse(pm)
@@ -417,9 +419,6 @@ class HIPBackend(BaseBackend):
         passes.common.add_canonicalizer(pm)
         passes.common.add_cse(pm)
         passes.common.add_symbol_dce(pm)
-
-        # This can not be moved below the di_scope pass
-        instrument(pm, name="proton", point="proton-to-llvm", options=options)
 
         if not knobs.compilation.disable_line_info and not knobs.compilation.dump_ir_extract_di_local_variables:
             passes.llvmir.add_di_scope(pm)
